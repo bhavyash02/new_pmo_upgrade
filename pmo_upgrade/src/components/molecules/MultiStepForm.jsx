6380d342affd809bd00cc2a05780ec0c62a58fce
bhavyash02 <bhavya.shah@accionlabs.com>
2025-03-17 10:44:33 +0530

    code formating

--- pmo_upgrade/src/components/molecules/MultiStepForm.jsx
+++ pmo_upgrade/src/components/molecules/MultiStepForm.jsx
@@ -7,76 +7,127 @@ import OneProtFolioDetails from "../../pages/PortFolioStatus/OneProtFolioDetails
 import ProgramPage from "../../pages/PortFolioStatus/ProgramPage";
 
 const stepLabels = [
-    "Portfolio Details",
-    "Program Details",
-    "Program Risk",
-    "Resources",
-    "Customer",
-    "Finance",
+  "Portfolio Details",
+  "Program Details",
+  "Program Risk",
+  "Resources",
+  "Customer",
+  "Finance",
 ];
 
-export default function MultipleStepForm({ setProtfolioStatus, protfolioStatus, newProspects, setNewProspects, newInitiatives, setNewInitiatives, avbPvbDetails, setAvbPvbDetails, valueAdds, setValueAdds, genAITech, setGenAITech, valueBoardEvaluation, setValueBoardEvaluation }) {
-    const [activeStep, setActiveStep] = useState(0);
+export default function MultipleStepForm({
+  setProtfolioStatus,
+  protfolioStatus,
+  inFlight,
+  setInFlight,
+  projectsOnTrack,
+  setProjectsOnTrack,
+  newProjects,
+  setNewProjects,
+  newProspects,
+  setNewProspects,
+  newInitiatives,
+  setNewInitiatives,
+  avbPvbDetails,
+  setAvbPvbDetails,
+  valueAdds,
+  setValueAdds,
+  genAITech,
+  setGenAITech,
+  valueBoardEvaluation,
+  setValueBoardEvaluation,
+}) {
+  const [activeStep, setActiveStep] = useState(0);
 
-    const handleNext = () => {
-        if (activeStep < stepLabels.length - 1) {
-            setActiveStep((prevStep) => prevStep + 1);
-        }
-    };
+  const handleNext = () => {
+    if (activeStep < stepLabels.length - 1) {
+      setActiveStep((prevStep) => prevStep + 1);
+    }
+  };
 
-    const handleBack = () => {
-        if (activeStep > 0) {
-            setActiveStep((prevStep) => prevStep - 1);
-        }
-    };
+  const handleBack = () => {
+    if (activeStep > 0) {
+      setActiveStep((prevStep) => prevStep - 1);
+    }
+  };
 
-    const renderStepContent = () => {
-        switch (activeStep) {
-            case 0:
-                return <OneProtFolioDetails protfolioStatus={protfolioStatus} setProtfolioStatus={setProtfolioStatus} />;
-            case 1:
-                return <ProgramPage newProspects={newProspects} setNewProspects={setNewProspects} newInitiatives={newInitiatives} setNewInitiatives={setNewInitiatives} avbPvbDetails={avbPvbDetails} setAvbPvbDetails={setAvbPvbDetails} valueAdds={valueAdds} setValueAdds={setValueAdds} genAITech={genAITech} setGenAITech={setGenAITech} valueBoardEvaluation={valueBoardEvaluation}
-                    setValueBoardEvaluation={setValueBoardEvaluation} />;
-            case 2:
-                return <h3>Program Risk</h3>;
-            case 3:
-                return <h3>Resources</h3>;
-            case 4:
-                return <h3>Customer</h3>;
-            case 5:
-                return <h3>Finance</h3>;
-            default:
-                return <h3>Unknown Step</h3>;
-        }
-    };
+  const renderStepContent = () => {
+    switch (activeStep) {
+      case 0:
+        return (
+          <OneProtFolioDetails
+            protfolioStatus={protfolioStatus}
+            setProtfolioStatus={setProtfolioStatus}
+          />
+        );
+      case 1:
+        return (
+          <ProgramPage
+            newProspects={newProspects}
+            setNewProspects={setNewProspects}
+            newInitiatives={newInitiatives}
+            setNewInitiatives={setNewInitiatives}
+            avbPvbDetails={avbPvbDetails}
+            setAvbPvbDetails={setAvbPvbDetails}
+            valueAdds={valueAdds}
+            setValueAdds={setValueAdds}
+            genAITech={genAITech}
+            setGenAITech={setGenAITech}
+            valueBoardEvaluation={valueBoardEvaluation}
+            setValueBoardEvaluation={setValueBoardEvaluation}
+          />
+        );
+      case 2:
+        return <h3>Program Risk</h3>;
+      case 3:
+        return <h3>Resources</h3>;
+      case 4:
+        return <h3>Customer</h3>;
+      case 5:
+        return <h3>Finance</h3>;
+      default:
+        return <h3>Unknown Step</h3>;
+    }
+  };
 
-    return (
-        <Box sx={{ width: "100%", display: "flex", flexDirection: "column" }}>
-            <Stepper activeStep={activeStep} alternativeLabel>
-                {stepLabels.map((label, index) => (
-                    <Step key={index}>
-                        <StepLabel>{label}</StepLabel>
-                    </Step>
-                ))}
-            </Stepper>
+  return (
+    <Box sx={{ width: "100%", display: "flex", flexDirection: "column" }}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {stepLabels.map((label, index) => (
+          <Step key={index}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
 
-            <Box sx={{ mt: 2, textAlign: "center", flexGrow: 1 }}>
-                {renderStepContent()}
-            </Box>
+      <Box sx={{ mt: 2, textAlign: "center", flexGrow: 1 }}>
+        {renderStepContent()}
+      </Box>
 
-            <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
-                <Button onClick={handleBack} disabled={activeStep === 0} variant="outlined">
-                    Back
-                </Button>
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
+        <Button
+          onClick={handleBack}
+          disabled={activeStep === 0}
+          variant="outlined"
+        >
+          Back
+        </Button>
 
-                <Button onClick={handleNext} disabled={
-                    (activeStep === 0 && !protfolioStatus) ||
-                    (activeStep === 1 && (!genAITech || !valueAdds || !avbPvbDetails || !valueBoardEvaluation))
-                } variant="contained">
-
-                    {activeStep === stepLabels.length - 1 ? "Finish" : "Next"}
-                </Button>
-            </Box>
-        </Box>
-    );
+        <Button
+          onClick={handleNext}
+          disabled={
+            (activeStep === 0 && !protfolioStatus) ||
+            (activeStep === 1 &&
+              (!genAITech ||
+                !valueAdds ||
+                !avbPvbDetails ||
+                !valueBoardEvaluation))
+          }
+          variant="contained"
+        >
+          {activeStep === stepLabels.length - 1 ? "Finish" : "Next"}
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- pmo_upgrade/src/pages/PortFolioStatus/index.jsx
+++ pmo_upgrade/src/pages/PortFolioStatus/index.jsx
@@ -4,82 +4,119 @@ import MultipleStepForm from "../../components/molecules/MultiStepForm";
 import { Box, Typography } from "@mui/material";
 
 function PortFolioStatus() {
-    const [buHead, setBuHead] = useState("");
-    const [engagementDirector, setEngagementDirector] = useState("");
-    const [deliveryDirector, setDeliveryDirector] = useState("");
-    const [deliveryManager, setDeliveryManager] = useState("");
-    const [handleSteps, setDataFromChild] = useState("");
-    const [protfolioStatus, setProtfolioStatus] = useState(null);
-    const [newProspects, setNewProspects] = useState(0);
-    const [newInitiatives, setNewInitiatives] = useState(0)
-    const [avbPvbDetails, setAvbPvbDetails] = useState("")
-    const [valueAdds, setValueAdds] = useState("")
-    const [genAITech, setGenAITech] = useState("")
-    const [valueBoardEvaluation, setValueBoardEvaluation] = useState("");
+  const [buHead, setBuHead] = useState("");
+  const [engagementDirector, setEngagementDirector] = useState("");
+  const [deliveryDirector, setDeliveryDirector] = useState("");
+  const [deliveryManager, setDeliveryManager] = useState("");
+  // section One
+  const [protfolioStatus, setProtfolioStatus] = useState(null);
+  const [inFlight, setInFlight] = useState(null);
+  const [projectsOnTrack, setProjectsOnTrack] = useState(null);
+  const [newProjects, setNewProjects] = useState(null);
+  // section Two
+  const [newProspects, setNewProspects] = useState(0);
+  const [newInitiatives, setNewInitiatives] = useState(0);
+  const [avbPvbDetails, setAvbPvbDetails] = useState("");
+  const [valueAdds, setValueAdds] = useState("");
+  const [genAITech, setGenAITech] = useState("");
+  const [valueBoardEvaluation, setValueBoardEvaluation] = useState("");
 
-    useEffect(() => {
-        console.log(handleSteps, 'handleSteps')
-    }, [handleSteps])
+  const dropdowns = ["Ramesh", "Lee", "Tony", "Kinesh"];
 
+  // Check if both required fields are selected
+  const isStepFormEnabled = deliveryDirector && deliveryManager;
 
-    const dropdowns = ["Ramesh", "Lee", "Tony", "Kinesh"];
+  return (
+    <Box sx={{ margin: 5 }}>
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{ fontWeight: 600, marginBottom: 10, marginRight: 150 }}
+      >
+        Add Portfolio Status For March 2025
+      </Typography>
 
-    // Check if both required fields are selected
-    const isStepFormEnabled = deliveryDirector && deliveryManager;
-
-    return (
-        <Box sx={{ margin: 5 }}>
-            <Typography variant="h4" gutterBottom sx={{ fontWeight: 600, marginBottom: 10, marginRight: 150 }}>
-                Add Portfolio Status For March 2025
-            </Typography>
-
-            <Box sx={{ display: "flex", gap: 4 }}>
-                {[
-                    { label: "BU Head", state: buHead, setState: setBuHead },
-                    { label: "Engagement Director", state: engagementDirector, setState: setEngagementDirector },
-                    { label: "Delivery Director", state: deliveryDirector, setState: setDeliveryDirector, required: true },
-                    { label: "Delivery Manager", state: deliveryManager, setState: setDeliveryManager, required: true },
-                ].map(({ label, state, setState, required }) => (
-                    <Box key={label} sx={{ display: "flex", flexDirection: "column", minWidth: 200 }}>
-                        <Typography sx={{ fontWeight: 500, marginBottom: 1, textAlign: "center", marginRight: "auto" }}>
-                            {label} {required && <span style={{ color: "red" }}>*</span>}
-                        </Typography>
-                        <Dropdown
-                            input={dropdowns}
-                            handleOnSelect={(event, newValue) => { setState(newValue); console.log(newValue) }}
-                            selectedValues={state}
-                        />
-                    </Box>
-                ))}
-            </Box>
-            {/* Stepper with Blur & Disable Effect */}
-            <Box
-                sx={{
-                    marginTop: 10,
-                    filter: isStepFormEnabled ? "none" : "blur(4px)",
-                    pointerEvents: isStepFormEnabled ? "auto" : "none", // Disable click
-                    opacity: isStepFormEnabled ? 1 : 0.5, // Reduce visibility
-                }}
+      <Box sx={{ display: "flex", gap: 4 }}>
+        {[
+          { label: "BU Head", state: buHead, setState: setBuHead },
+          {
+            label: "Engagement Director",
+            state: engagementDirector,
+            setState: setEngagementDirector,
+          },
+          {
+            label: "Delivery Director",
+            state: deliveryDirector,
+            setState: setDeliveryDirector,
+            required: true,
+          },
+          {
+            label: "Delivery Manager",
+            state: deliveryManager,
+            setState: setDeliveryManager,
+            required: true,
+          },
+        ].map(({ label, state, setState, required }) => (
+          <Box
+            key={label}
+            sx={{ display: "flex", flexDirection: "column", minWidth: 200 }}
+          >
+            <Typography
+              sx={{
+                fontWeight: 500,
+                marginBottom: 1,
+                textAlign: "center",
+                marginRight: "auto",
+              }}
             >
-                <MultipleStepForm
-                    setProtfolioStatus={setProtfolioStatus}
-                    protfolioStatus={protfolioStatus}
-                    newProspects={newProspects}
-                    setNewProspects={setNewProspects}
-                    newInitiatives={newInitiatives}
-                    setNewInitiatives={setNewInitiatives}
-                    avbPvbDetails={avbPvbDetails}
-                    setAvbPvbDetails={setAvbPvbDetails}
-                    valueAdds={valueAdds}
-                    setValueAdds={setValueAdds}
-                    genAITech={genAITech}
-                    setGenAITech={setGenAITech}
-                    valueBoardEvaluation={valueBoardEvaluation}
-                    setValueBoardEvaluation={setValueBoardEvaluation} />
-                {/* <OneProtFolioDetails nextStep={steps} /> */}
-            </Box>
-        </Box>
-    );
+              {label} {required && <span style={{ color: "red" }}>*</span>}
+            </Typography>
+            <Dropdown
+              input={dropdowns}
+              handleOnSelect={(event, newValue) => {
+                setState(newValue);
+                console.log(newValue);
+              }}
+              selectedValues={state}
+            />
+          </Box>
+        ))}
+      </Box>
+      {/* Stepper with Blur & Disable Effect */}
+      <Box
+        sx={{
+          marginTop: 10,
+          filter: isStepFormEnabled ? "none" : "blur(4px)",
+          pointerEvents: isStepFormEnabled ? "auto" : "none", // Disable click
+          opacity: isStepFormEnabled ? 1 : 0.5, // Reduce visibility
+        }}
+      >
+        <MultipleStepForm
+          setProtfolioStatus={setProtfolioStatus}
+          protfolioStatus={protfolioStatus}
+          inFlight={inFlight}
+          setInFlight={setInFlight}
+          projectsOnTrack={projectsOnTrack}
+          setProjectsOnTrack={setProjectsOnTrack}
+          newProjects={newProjects}
+          setNewProjects={setNewProjects}
+          newProspects={newProspects}
+          setNewProspects={setNewProspects}
+          newInitiatives={newInitiatives}
+          setNewInitiatives={setNewInitiatives}
+          avbPvbDetails={avbPvbDetails}
+          setAvbPvbDetails={setAvbPvbDetails}
+          valueAdds={valueAdds}
+          setValueAdds={setValueAdds}
+          genAITech={genAITech}
+          setGenAITech={setGenAITech}
+          valueBoardEvaluation={valueBoardEvaluation}
+          setValueBoardEvaluation={setValueBoardEvaluation}
+        />
+        {/* <OneProtFolioDetails nextStep={steps} /> */}
+      </Box>
+    </Box>
+  );
 }
 
 export default PortFolioStatus;
